7f8f5634c960ca5a9c4df68bd9ae8bf8f5252a17
Vitor Dilucca <vitordilucca@hotmail.com>
2023-11-07 17:47:54 -0300

    id nao é mais necessaria no component Item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,16 +1,12 @@
-import { ItemId } from "../App";
-
 export function Item({
-  id,
   text,
   handleClick,
 }: {
-  id: ItemId;
   text: string;
   handleClick: () => void;
 }) {
   return (
-    <li key={id}>
+    <li>
       {text}
       <button onClick={handleClick}>Eliminar elemento</button>
     </li>
